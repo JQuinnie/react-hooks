9da09736763fab8334181707a59f664e08f6db9b
jquinnie <jenn8866@gmail.com>
2020-04-15 16:23:17 -0600

    Create route error landing component

---
+++ src/components/error-page.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Footer from './footer.component';
+
+export default () => (
+  <div>
+    <p className="text-center text-4xl text-red-600 font-bold p-4">You Found This By Accident,<br /> uhoh, 404!</p>
+    <Footer />
+  </div>
+);
