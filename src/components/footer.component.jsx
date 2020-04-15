73d117c967dac57f53edd41420cf30463b668312
jquinnie <jenn8866@gmail.com>
2020-04-15 16:23:06 -0600

    Add more styles to footer

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -5,7 +5,12 @@ const Footer = () => (
   <div>
     <footer>
       <Link to="/">
-        <button style={{ cursor: 'pointer' }}>Go Home</button>
+        <button
+          className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded"
+          style={{ cursor: 'pointer' }}
+        >
+          Go Home
+        </button>
       </Link>
     </footer>
   </div>
